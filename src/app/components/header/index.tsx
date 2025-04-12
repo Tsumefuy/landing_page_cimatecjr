eedbda405ccc0ff356b33e115fe9bab269272eda
Gustavo <maiagustavo125@gmail.com>
2025-04-11 23:45:15 -0300

    use Link

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -3,6 +3,7 @@
 import { usePathname } from "next/navigation";
 import { FaBars, FaXmark } from "react-icons/fa6";
 import { useState } from "react";
+import Link from "next/link";
 
 export function Header() {
     const pathname = usePathname();
@@ -13,24 +14,22 @@ export function Header() {
         { name: "CATÁLOGO", path: "/catalogo" },
     ];
 
-
-
     return (
         <header className="absolute left-0 w-full h-20 flex items-center justify-between px-6 md:px-20 py-4 bg-background border-b border-black z-50">
-            <a href="/" className="flex items-center text-2xl">
+            <Link href="/" className="flex items-center text-2xl">
                 <img src="icon.png" alt="Portal Gun" className="h-8 mr-3 mb-1" />
                 <span className="text-primary font-extrabold">PortalGun</span>
                 <span className="text-secundary font-normal">Store</span>
-            </a>
+            </Link>
             <nav className={`md:flex items-center space-x-8 md:space-x-12 text-secundary text-base font-normal 
                 ${openMenu ? "block absolute top-20 right-0 w-full transform transition-all duration-300 ease-in-out bg-background shadow-md p-4" : "hidden"} `}>
                 {navLinks.map((link) => (
-                    <a key={link.path} href={link.path} className="relative group hover:text-gray-500 transition-all duration-300">
+                    <Link key={link.path} href={link.path} className="relative group hover:text-gray-500 transition-all duration-300">
                         {link.name}
                         <span className={`absolute left-0 bottom-0 w-full h-0.5 bg-green-500 transition-transform ${
                             pathname === link.path ? "scale-x-100" : "scale-x-0 group-hover:scale-x-100"
                         }`}></span>
-                    </a>
+                    </Link>
                 ))}
             </nav>
             <button className="md:hidden" onClick={() => setOpenMenu(!openMenu)}>
